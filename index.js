2ff00a8eb1e51706f52834246cc8920fa2a34415
Rajajaiswal08 <jaiswalraja871@gmail.com>
2025-03-25 20:46:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const AdminRouter = require('./routers/AdminRouter');
+const DoctourRouter = require('./routers/DoctourRouters');
+const BillRouter = require('./routers/BillRouter');
+const PackageRouter = require('./routers/packageRouter');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
+// Routes
+app.use('/admin', AdminRouter);
+app.use('/DoctorCategory', DoctourRouter);
+app.use('/Package', PackageRouter);
+app.use('/bill', BillRouter);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONOGOB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('DB connected');
+    app.listen(5000, () => {
+        console.log('Server started on http://localhost:5000');
+    });
+})
+.catch((error) => {
+    console.log('Unable to connect to DB:', error.message);
+});
